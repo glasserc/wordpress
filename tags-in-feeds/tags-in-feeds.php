abad550e065f626ab2487127f3e1184a987e95f0
Ethan Glasser-Camp <ethan@betacantrips.com>
2009-11-10 22:49:02 -0500

    Make this actually work

--- tags-in-feeds/tags-in-feeds.php
+++ tags-in-feeds/tags-in-feeds.php
@@ -1,8 +1,33 @@
 <?php
+/*
+Plugin Name: Add tags to RSS feeds
+Plugin URI: http://
+Description: Add links to the tags in each post
+Version: 0.1
+Author: Ethan Glasser-Camp
+Author URI: http://
+*/
+
+/*  Copyright YEAR  PLUGIN_AUTHOR_NAME  (email : PLUGIN AUTHOR EMAIL)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
 add_filter("get_the_generator_atom", "tags_in_feeds_is_atom", 8);
 add_filter("get_the_generator_rss2", "tags_in_feeds_is_rss2", 8);
-add_filter("get_the_content", "tags_in_feeds_filter_the_content", 0);
+add_filter("the_content", "tags_in_feeds_filter_the_content", 0);
 
 $TYPE = null;
 function tags_in_feeds_is_atom($data){
@@ -11,18 +36,22 @@ function tags_in_feeds_is_atom($data){
   return $data;
 }
 
-function tags_in_feeds is_rss2($data){
+function tags_in_feeds_is_rss2($data){
   global $TYPE;
   $TYPE = 'rss2';
   return $data;
 }
 
 function tags_in_feeds_filter_the_content($data){
-  global $TYPE, $post;
+  global $TYPE;
   if($TYPE == null) return $data;
-  $tags = array();
-  foreach($post->tags as $tag){
-    $tags[] = "<a href=\"\">". $tag->name . "</a>";
+  $post = get_post(get_the_ID());
+  $terms = get_the_tags();
+
+  $tags_array = array();
+  foreach($terms as $tag){
+    $tags_array[] = "<a href='" . get_tag_link($tag) . "'>" . $tag->name . "</a>";
   }
-  $tags_html = "<p>Filed under: " . implode($tags, ",") . "</p>";
+  $tags_html = "<p>Filed under: " . implode(', ', $tags_array)  . "</p>";
+  return $tags_html . $data;
 }
